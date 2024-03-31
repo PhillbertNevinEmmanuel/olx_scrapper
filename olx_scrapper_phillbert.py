b21291d733e709de9f76fca7177e35dc6a1d1242
Phillbert Nevin Emmanuel <75604499+PhillbertNevinEmmanuel@users.noreply.github.com>
2024-03-31 15:46:39 +0700

    Add files via upload

---
+++ olx_scrapper_phillbert.py
@@ -0,0 +1,38 @@
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximalized")
+url = 'https://www.olx.co.id/jakarta-selatan_g4000030/q-mobil'
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get(url)
+
+for i in range(100):
+    time.sleep(3)
+    try:
+        driver.find_element(By.CSS_SELECTOR, "div._38O09 > button").click()
+        time.sleep(3)
+    except NoSuchElementException:
+        break
+time.sleep(5)
+
+products = []
+soup = BeautifulSoup(driver.page_source, "html_parser")
+for item in soup.findAll('li', class_ = '_1DNjI'):
+    product_name = item.find('span', class_= '_2poNJ').text
+    price = item.find('span', class_='_2Ks63').text
+    products.append((product_name, price))
+
+df = pd.DataFrame(products, columns=['Car Name', 'Price'])
+print(df)
+
+df.to_csv(r'OLX Scrapper.csv', index=False)
+print('Data saved to local disk!')
+
+driver.close()
+
